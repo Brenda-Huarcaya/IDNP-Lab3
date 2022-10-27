77f2b04e2766432e170e32a248e051098a764c19
Brenda-Huarcaya <bhuarcaya@unsa.edu.pe>
2022-10-26 19:45:07 -0500

    Activity Main es el Activity Login - COMPLETO

--- app/src/main/java/com/idnp/myapplication/MainActivity.java
+++ app/src/main/java/com/idnp/myapplication/MainActivity.java
@@ -3,11 +3,14 @@ package com.idnp.myapplication;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,18 +25,31 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button startButton = findViewById(R.id.btnActivityMenu);
+        Button loginButton = findViewById(R.id.btnLogin);
+        EditText eUsuario = findViewById(R.id.eUsuario);
+        EditText eContrasena = findViewById(R.id.eContraseña);
+        TextView tMensaje = findViewById(R.id.eMensaje);
 
-        startButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-                startActivity(intent);
+                String usuario = eUsuario.getText().toString();
+                String contrasena = eContrasena.getText().toString();
+
+                if (usuario.equals("usuario") && contrasena.equals("12345")){
+                    Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    tMensaje.setText("Usuario Invalido");
+                }
+
             }
         });
 
